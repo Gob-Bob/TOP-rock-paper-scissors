2f779901368fc992ee2c966e5bdf710e0b916d58
Tyler <nguyentyler97@gmail.com>
2022-06-29 20:20:52 -0700

    Write pseudocode in javascript file

---
+++ javascript.js
@@ -0,0 +1,7 @@
+// Get player's input
+    // Only accept the answers of rock, paper, or scissors (cap non-sensitive)
+// Get computer's input
+    // Randomly generate the computer's input between rock, paper and scissors
+// Function compares both player's inputs
+    // Determine which input beats the other
+    // Whoever's input beats the other, declare them the winner
