03e1e1fa7ed894f34240296f00694334d88090d7
Tyler <nguyentyler97@gmail.com>
2022-06-29 21:07:24 -0700

    Enable loop for user input if incorrect answer

--- javascript.js
+++ javascript.js
@@ -1,20 +1,20 @@
 /* If user input is not rock, paper, or scissors, 
     throw an error message and ask them for the input again*/
-
-// Get player's input
-let playerChoice = prompt("Rock, paper, or scissors?");
-// Convert entire string it lowercase to make user input cap non-sensitive
-playerChoice = playerChoice.toLowerCase();
-
-// If player input is rock, paper or scissors, continue
-if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
-    console.log("Continue");
-} else {
-    // If player input is not rock, paper or scissors, abort
-    alert("Please input as indicated");
-    console.log("Failed");
+while (true) {
+    // Ask user for input
+    let playerChoice = prompt("Rock, paper, or scissors?");
+    // Convert entire string it lowercase to make user input cap non-sensitive
+    playerChoice = playerChoice.toLowerCase();
+    // If user input does not equal choices
+    if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
+        // Break out of loop
+        break;
+    } else {
+        // Contine the loop again
+        alert("Please input rock, paper, or sciessors")
+        continue;
+    }
 }
-
 // Get computer's input
     // Randomly generate the computer's input between rock, paper and scissors
 // Function compares both player's inputs
