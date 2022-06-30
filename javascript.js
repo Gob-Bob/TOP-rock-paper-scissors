de5cbc1236f2960f3ac6904a840813fe6272da00
Tyler <nguyentyler97@gmail.com>
2022-06-29 20:49:18 -0700

    Enable user input

--- javascript.js
+++ javascript.js
@@ -1,5 +1,20 @@
+/* If user input is not rock, paper, or scissors, 
+    throw an error message and ask them for the input again*/
+
 // Get player's input
-    // Only accept the answers of rock, paper, or scissors (cap non-sensitive)
+let playerChoice = prompt("Rock, paper, or scissors?");
+// Convert entire string it lowercase to make user input cap non-sensitive
+playerChoice = playerChoice.toLowerCase();
+
+// If player input is rock, paper or scissors, continue
+if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
+    console.log("Continue");
+} else {
+    // If player input is not rock, paper or scissors, abort
+    alert("Please input as indicated");
+    console.log("Failed");
+}
+
 // Get computer's input
     // Randomly generate the computer's input between rock, paper and scissors
 // Function compares both player's inputs
